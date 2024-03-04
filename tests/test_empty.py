87a2b88dda48a95d55521151cda0a46a697c1b63
Ryan Wood <ryan.wood@electroroute.co.jp>
2024-03-04 13:59:37 +0900

    Realizing that my poetry project name was bad

---
+++ tests/test_empty.py
@@ -0,0 +1,14 @@
+"""Empty test.
+
+Remove when code.
+"""
+
+from mms_client.empty import is_empty
+
+
+def test_everything():
+    """Empty test.
+
+    Remove when code.
+    """
+    assert is_empty()
