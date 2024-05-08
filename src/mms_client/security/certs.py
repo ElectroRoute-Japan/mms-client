e9445ba9680a86682bef86f78c51bcf15efa42e3
Ryan Wood <ryan.wood@electroroute.co.jp>
2024-05-08 09:45:39 +0900

    Need to fix signature verification

--- src/mms_client/security/certs.py
+++ src/mms_client/security/certs.py
@@ -3,11 +3,20 @@
 from pathlib import Path
 from typing import Union
 
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.hazmat.primitives.serialization import NoEncryption
+from cryptography.hazmat.primitives.serialization import PrivateFormat
+from cryptography.hazmat.primitives.serialization import PublicFormat
+from cryptography.hazmat.primitives.serialization.pkcs12 import load_key_and_certificates
 from requests_pkcs12 import Pkcs12Adapter
 
 
 class Certificate:
     """Describes a certificate composed of a cert file and a key file."""
+
+    # The encoding to use for MMS certificates
+    encoding = Encoding.PEM
 
     def __init__(self, cert: Union[str, Path, bytes], passphrase: str):
         """Create a new Certificate.
@@ -29,6 +38,13 @@ class Certificate:
         # Save the passphrase
         self._passphrase = passphrase
 
+        # Load the private key using the cryptography library
+        private_key, _, _ = load_key_and_certificates(self._cert, self._passphrase.encode("UTF-8"))
+        if isinstance(private_key, RSAPrivateKey):
+            self._private = private_key
+        else:
+            raise ValueError(f"Private key of type ({type(self._private).__name__}) was not expected.")
+
     @property
     def certificate(self) -> bytes:
         """Return the certificate data."""
@@ -38,6 +54,22 @@ class Certificate:
     def passphrase(self) -> str:
         """Return the full path to the passphrase."""
         return self._passphrase
+
+    def public_key(self) -> bytes:
+        """Extract the public key from the certificate.
+
+        Returns: The public key in PEM format.
+        """
+        return self._private.public_key().public_bytes(Certificate.encoding, PublicFormat.PKCS1)
+
+    def private_key(self) -> bytes:
+        """Extract the private key from the certificate.
+
+        THIS SHOULD NOT, UNDER ANY CIRCUMSTANCES, BE PRINTED OR LOGGED!!!
+
+        Returns: The private key in PEM format.
+        """
+        return self._private.private_bytes(Certificate.encoding, PrivateFormat.TraditionalOpenSSL, NoEncryption())
 
     def to_adapter(self) -> Pkcs12Adapter:
         """Convert the certificate to a Pkcs12Adapter."""
--- src/mms_client/security/crypto.py
+++ src/mms_client/security/crypto.py
@@ -1,13 +1,11 @@
 """Contains objects for cryptographic operations."""
 
+from base64 import b64decode
 from base64 import b64encode
-from hashlib import sha256
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
-from cryptography.hazmat.primitives.serialization.pkcs12 import load_key_and_certificates
+from Cryptodome.Hash import SHA256
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Signature import PKCS1_v1_5
 
 from mms_client.security.certs import Certificate
 
@@ -21,23 +19,28 @@ class CryptoWrapper:
         Arguments:
         cert (Certificate): The certificate to use for cryptographic operations.
         """
-        # First, save our certificate for later use
-        self._cert = cert
+        # Extract the public key and private key data from the certificate
+        private_key = RSA.import_key(cert.private_key())
+        public_key = RSA.import_key(cert.public_key())
 
-        # Next, import the private key from the certificate
-        private_key, _, _ = load_key_and_certificates(
-            self._cert.certificate, self._cert.passphrase.encode(), default_backend()
-        )
+        # Create a new signer from the private key and a new verifier from the public key
+        self._signer = PKCS1_v1_5.new(private_key)
+        self._verifier = PKCS1_v1_5.new(public_key)
 
-        # Now, we need to assert typing on this private key to make mypy happy
-        if isinstance(private_key, RSAPrivateKey):
-            self._private_key = private_key
-        else:
-            raise TypeError(f"Private key of type ({type(private_key).__name__}) was not expected.")
+    def verify(self, content: bytes, signature: bytes) -> bool:
+        """Verify a signature against the given content using the certificate.
 
-        # Finally, save our padding and algorithm for later use
-        self._padding = padding.PKCS1v15()
-        self._algorithm = hashes.SHA256()
+        Arguments:
+        content (bytes):    The content to verify.
+        signature (bytes):  The signature to verify against the content.
+
+        Returns:    True if the signature is valid, False otherwise.
+        """
+        # Hash the content using SHA256
+        hashed = SHA256.new(content)
+
+        # Verify the signature using the public key
+        return self._verifier.verify(hashed, b64decode(signature))
 
     def sign(self, data: bytes) -> bytes:
         """Create a signature from the given data using the certificate.
@@ -48,10 +51,10 @@ class CryptoWrapper:
         Returns:    A base64-encoded string containing the signature.
         """
         # First, hash the data using SHA256
-        hashed = sha256(data)
+        hashed = SHA256.new(data)
 
         # Next, sign the hash using the private key
-        signature = self._private_key.sign(hashed.digest(), self._padding, self._algorithm)
+        signature = self._signer.sign(hashed)
 
         # Finally, return the base64-encoded signature
         return b64encode(signature)
