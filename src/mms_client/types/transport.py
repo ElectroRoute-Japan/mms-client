d82dfed4eba52125dd8e3739538d705adcd8fb6a
Ryan Wood <ryan.wood@electroroute.co.jp>
2024-06-13 12:38:02 +0900

    Fix the request type for registration

--- src/mms_client/types/transport.py
+++ src/mms_client/types/transport.py
@@ -17,7 +17,7 @@ class RequestType(Enum):
 
     INFO = "mp.info"
     MARKET = "mp.market"
-    REGISTRATION = "mpr"
+    REGISTRATION = "mp.registration"
     REPORT = "mp.report"
     OMI = "mp.omi"
 
